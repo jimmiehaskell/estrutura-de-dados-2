6150869cddeecc53183ef095d4a335c8ea937975
Jimmie Haskell <haskell4228@gmail.com>
2025-08-20 22:45:30 -0300

    Iniciado a versão 2.0, aplicação de POO.

---
+++ Aluno.cpp
@@ -0,0 +1,90 @@
+#include "Aluno.hpp"
+
+#include <iomanip>
+#include <iostream>
+#include <ostream>
+
+// métodos privados
+void Aluno::setAlunoMedia(std::vector<float> _notas, float _mediaAprovacao) {
+    float somaNotas = 0;
+    for (int i = 0; i < _notas.size(); i++) {
+        somaNotas += _notas[i];
+    }
+
+    this->alunoMedia = somaNotas / _notas.size();
+    setAlunoAprovado(this->alunoMedia > _mediaAprovacao);
+}
+
+void Aluno::setAlunoAprovado(bool a) {
+    this->alunoAprovado = a;
+}
+
+/*
+ * @param a, primeiro aluno a ser comparado
+ * @param b, segundo aluno a ser comparado
+ * @param o, (true ou false) representa a ordenação,
+ * true = decrescente, false = crescente
+*/
+bool Aluno::compararMedias(const Aluno &a, const Aluno &b, bool o) {
+    if (o) {
+        return a.alunoMedia > b.alunoMedia;
+    }
+    return a.alunoMedia < b.alunoMedia;
+}
+
+// métodos publicos
+// getters
+std::string Aluno::getAlunoNome() {
+    return alunoNome;
+}
+
+std::vector<float> Aluno::getAlunoNotas() {
+    return alunoNotas;
+}
+
+char Aluno::getAlunoSexo() {
+    return alunoSexo;
+}
+
+float Aluno::getAlunoMedia() {
+    return alunoMedia;
+}
+
+std::string Aluno::getAlunoAprovado() {
+    if (this->alunoAprovado) {
+        return "Aprovado!";
+    }
+    return "Reprovado!";
+}
+
+// setters
+void Aluno::setAlunoNome(std::string nome) {
+    this->alunoNome = nome;
+}
+
+void Aluno::setAlunoNotas(std::vector<float> _notas, float _mediaAprovacao) {
+    if (_notas.size() > 0) {
+        this->alunoNotas = _notas;
+        setAlunoMedia(_notas, _mediaAprovacao);
+    }
+}
+
+
+void Aluno::setAlunoSexo(char sexo) {
+    this->alunoSexo = sexo;
+}
+
+// int main() {
+//     Aluno aluno1;
+//     std::vector<float> aluno1Notas = {8, 9.75, 7.53, 9, 10};
+//     aluno1.setAlunoNome("Jimmie");
+//     aluno1.setAlunoNotas(aluno1Notas, 6);
+//     aluno1.setAlunoSexo('M');
+//
+//     std::cout << "Aluno: " << aluno1.getAlunoNome() << std::endl;
+//     std::cout << std::fixed << std::setprecision(2);
+//     std::cout << "\tMédia: " << aluno1.getAlunoMedia() << std::endl;
+//     std::cout << "\tStatus: " << aluno1.getAlunoAprovado() << std::endl;
+//
+//     return 0;
+// }
---
+++ Aluno.hpp
@@ -0,0 +1,40 @@
+#ifndef ALUNO_HPP
+#define ALUNO_HPP
+#include <string>
+#include <vector>
+
+
+class Aluno {
+private:
+    // variáveis
+    std::string alunoNome;
+    std::vector<float> alunoNotas;
+    char alunoSexo;
+    float alunoMedia;
+    bool alunoAprovado;
+
+    // métodos
+    void setAlunoMedia(std::vector<float> _notas, float _mediaAprovacao);
+    void setAlunoAprovado(bool a);
+    bool compararMedias(const Aluno &a, const Aluno &b, bool o);
+
+public:
+    // construtor
+    Aluno() : alunoMedia(0), alunoAprovado(false){};
+
+    // métodos
+    // getters
+    std::string getAlunoNome();
+    std::vector<float> getAlunoNotas();
+    char getAlunoSexo();
+    float getAlunoMedia();
+    std::string getAlunoAprovado();
+
+    // setters
+    void setAlunoNome(std::string nome);
+    void setAlunoNotas(std::vector<float> _notas, float _mediaAprovacao);
+    void setAlunoSexo(char sexo);
+};
+
+
+#endif // ALUNO_HPP
